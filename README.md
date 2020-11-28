23ce8a3ebf213f06f735624542f30aeab59d9db2
Aleksei Burov <burov_alexe@mail.ru>
2020-11-28 12:37:58 +0500

    An initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Enki (Godot network plugin for conecting with KBEngine)
+
+## Development
+
+Install pyenv
+
+```
+PROJ_DIR=<YOUR_PROJ_DIR>
+git clone git@github.com:ve-i-uj/enki.git $PROJ_DIR
+cd $PROJ_DIR
+bash scripts/makepyvenv.sh
+```
---
+++ scripts/makepyvenv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Cretes python virtual environment
+#
+
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source `realpath $curr_dir/init.sh`
+
+python3 -m pip install -U pip
+python3 -m pip install -U virtualenv
+
+if [ ! -d "$PYVENV_DIR" ]; then
+	mkdir -p $PYVENV_DIR
+fi
+
+python3 -m venv "$PYVENV_DIR/$SERVICE_NAME"
