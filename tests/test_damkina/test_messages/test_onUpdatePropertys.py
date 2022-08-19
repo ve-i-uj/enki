539d318819ad954cba4c18379111468a84f0ee03
Aleksei Burov <burov_alexey@mail.ru>
2022-08-19 11:24:39 +0500

    A test for the "onEntityDestroyed" message was added + minor updates

---
+++ tests/test_damkina/test_messages/test_onUpdatePropertys.py
@@ -0,0 +1,46 @@
+import unittest
+
+from damkina import apphandler, entitymgr, appl
+from enki import kbeclient, descr, settings
+from enki.interface import IMessage, IMsgReceiver
+
+
+class _MsgReceiver(IMsgReceiver):
+
+    def __init__(self) -> None:
+        self.msgs: dict[int, IMessage] = {}
+
+    def on_receive_msg(self, msg: IMessage) -> bool:
+        self.msgs[msg.id] = msg
+
+
+class OnUpdatePropertysTestCase(unittest.TestCase):
+    """Test onUpdatePropertys"""
+
+    def setUp(self):
+        super().setUp()
+        login_app_addr = settings.AppAddr('0.0.0.0', 20013)
+        self._app = appl.App(login_app_addr, server_tick_period=5)
+        self._entity_mgr: entitymgr.EntityMgr = entitymgr.EntityMgr(self._app)
+
+    def test_ok(self):
+        data = b'\xff\x01\x0e\x00\x81\x08\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\xf8\x01\x14\x00\x00\x00\x07\x00\x95\x84\xfbb\x81\x08\x00\x00Account\x00'
+        msg, data_tail = kbeclient.Serializer().deserialize(memoryview(data))
+        assert msg is not None, 'Invalid initial data'
+
+        self._entity_mgr.initialize_entity(2177, 'Account')
+        handler = apphandler.OnUpdatePropertysHandler(self._entity_mgr)
+        result: apphandler.HandlerResult = handler.handle(msg)
+        assert result.success
+
+    def test_on_update_before_on_created_proxy(self):
+        data = b'\xff\x01\x0e\x00\xf3\x00\x00\x00\x00\x04\x02\x00\x00\x00\x00\x00\x00\x00\xf8\x01\x14\x00\x00\x00\x07\x00\xf98\xfeb\xf3\x00\x00\x00Account\x00'
+        msg_511, data = kbeclient.Serializer().deserialize(memoryview(data))
+        msg_504, data_tail = kbeclient.Serializer().deserialize(memoryview(data))
+        assert msg_511 and msg_504, 'Invalid initial data'
+
+        apphandler.OnUpdatePropertysHandler(self._entity_mgr).handle(msg_511)
+
+        handler = apphandler.OnCreatedProxiesHandler(self._entity_mgr)
+        result: apphandler.HandlerResult = handler.handle(msg_504)
+        assert result.success
