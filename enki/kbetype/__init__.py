b23712e432d0a128477e063bada622ee7d9f97a5
Aleksei Burov <burov_alexey@mail.ru>
2026-02-22 11:25:01 +0500

    Все декодеры и декод-ые типы доступны из enki.kbetype

--- enki/kbetype/__init__.py
+++ enki/kbetype/__init__.py
@@ -1,1 +1,5 @@
 """Пакет со всем, что связано с типами KBEngine."""
+
+from .decoders import *
+from .ikbetype import *
+from .pytypes import *
--- enki/kbetype/pytypes/__init__.py
+++ enki/kbetype/pytypes/__init__.py
@@ -1,1 +1,6 @@
 """Типы на python соответствующие сетевым типам KBEngine."""
+
+from .basic_data_types import *
+from .collections import *
+from .entity_component import *
+from .vectors import *
